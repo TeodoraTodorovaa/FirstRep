5489a4593b7a0d8501ceac21b8c1613693028070
usagichi <geri_ivanova_@abv.bg>
2021-03-23 23:48:33 +0200

    Third commit to Tedi's repository.

--- UspProject/src/Main.java
+++ UspProject/src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("PC2 strikes again! Ho-ho!");
+		System.out.println("PC2 is trying to make a conflict!");
 	}
 
 }
